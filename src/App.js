2ea871ae21805fa10df9f7f14d2c16dea449f5ff
VishuKaier2003 <vishukalier123@gmail.com>
2023-11-07 13:40:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
+  const data = fetch('https://practice-crud-404316.et.r.appspot.com/')
+  console.log(data+"Fetched from https://practice-crud-404316.et.r.appspot.com/");
   return (
-    <div>
+    <div>Hello World
     <BrowserRouter>
       <Router>
         <Route path="/"></Route>
       </Router>
     </BrowserRouter>
+    <div>
+    </div>
     </div>
   );
 }
